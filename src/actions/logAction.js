d1b8bc8179f6fd07bf5be20bb9734fa8d5933028
nikhilshankar96 <nikhilshankar96@hotmail.com>
2020-02-05 13:19:18 -0500

    search func added

--- src/actions/logAction.js
+++ src/actions/logAction.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+	GET_LOGS,
+	SET_LOADING,
+	LOGS_ERROR,
+	ADD_LOG,
+	DELETE_LOG,
+	UPDATE_LOG,
+	SEARCH_LOGS,
+	SET_CURRENT,
+	CLEAR_CURRENT
+} from "./types";
 
 export const getLogs = () => async dispatch => {
 	try {
@@ -43,7 +53,78 @@ export const addLog = log => async dispatch => {
 	}
 };
 
-//set loading true
+export const deleteLog = id => async dispatch => {
+	try {
+		setLoading();
+
+		await fetch(`/logs/${id}`, {
+			method: "DELETE"
+		});
+
+		dispatch({
+			type: DELETE_LOG,
+			payload: id
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+export const updateLog = log => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs/${log.id}`, {
+			method: "PUT",
+			body: JSON.stringify(log),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: UPDATE_LOG,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+export const searchLogs = text => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs?q=${text}`);
+		const data = await res.json();
+
+		dispatch({
+			type: SEARCH_LOGS,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+export const setCurrent = log => {
+	return { type: SET_CURRENT, payload: log };
+};
+
+export const clearCurrent = () => {
+	return { type: CLEAR_CURRENT };
+};
+
 export const setLoading = () => {
 	return {
 		type: SET_LOADING
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { updateLog } from "../../actions/logAction";
+import PropTypes from "prop-types";
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
 	const [message, setMessage] = useState("");
 	const [attention, setAttention] = useState(false);
 	const [tech, setTech] = useState("");
+
+	useEffect(() => {
+		if (current) {
+			setMessage(current.message);
+			setAttention(current.attention);
+			setTech(current.tech);
+		}
+	}, [current]);
 
 	const onSubmit = () => {
 		if (message === "" || tech === "") {
 			M.toast({ html: "Please enter a message and tech" });
 		} else {
-			console.log(message, tech, attention);
+			// console.log(message, tech, attention);
+			const updLog = {
+				id: current.id,
+				message,
+				attention,
+				tech,
+				date: new Date()
+			};
+
+			updateLog(updLog);
+			M.toast({ html: `Log updated by ${tech}` });
+
 			//clear fields
 			setMessage("");
 			setTech("");
@@ -30,9 +52,6 @@ const EditLogModal = () => {
 							value={message}
 							onChange={e => setMessage(e.target.value)}
 						/>
-						<label htmlFor='message' className='active'>
-							Log message
-						</label>
 					</div>
 				</div>
 
@@ -95,4 +114,13 @@ const modalStyle = {
 	height: "75%"
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+	current: PropTypes.object,
+	updatelog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+	current: state.log.current
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
